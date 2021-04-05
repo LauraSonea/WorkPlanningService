ed72065be84c9d6fb8c5c70c1f82006cceca8db0
laura.sonea <laura.sonea@DESKTOP-6UM78D2>
2021-04-05 14:03:49 +0300

    delete proj

--- WorkPlanningApi/WorkPlanningApi.Messaging.Send/Sender/v1/IShiftUpdateSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using WorkPlanningApi.Domain.Entities;
-
-namespace WorkPlanningApi.Messaging.Send.Sender.v1
-{
-   public interface IShiftUpdateSender
-    {
-        void SendShift(Shift shift);
-    }
-}
